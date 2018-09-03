3bc5475df083c5453adcbe4a86c9350cc61b62e2
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-09-03 09:13:03 +0200

    Add project files.

---
+++ PM.Gui/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using PM.EF;
+
+namespace PM.Gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ProjectManagementDBEntities model = new ProjectManagementDBEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            Project project = new Project();
+            project.Name = textBoxProjectName.Text;
+            project.Description = textBoxDescription.Text;
+            model.Projects.Add(project);
+            model.SaveChanges();
+            dataGridContactInfo.ItemsSource = model.Projects.ToList();
+        }
+    }
+}
