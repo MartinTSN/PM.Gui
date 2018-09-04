a5072ce75c4c3b29a5bb29bb116b2b51f20022bf
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-09-04 09:02:26 +0200

    outcommented

--- PM.EF/Project.cs
+++ PM.EF/Project.cs
@@ -11,7 +11,7 @@ namespace PM.EF
 {
     using System;
     using System.Collections.Generic;
-
+    
     public partial class Project
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,12 +19,12 @@ namespace PM.EF
         {
             this.Teams = new HashSet<Team>();
         }
-
+    
         public int Id { get; set; }
-        //  public string Name { get; set; }
-        //   public string Description { get; set; }
-
-        //  [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        //  public virtual ICollection<Team> Teams { get; set; }
+      //  public string Name { get; set; }
+      //  public string Description { get; set; }
+    
+       // [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<Team> Teams { get; set; }
     }
 }
