c98f985d7887e66e58d80621c8fbd635965e8f3e
Martin Thue Schilling Nissen <mart038e@edu.campusvejle.dk>
2018-09-05 09:24:23 +0200

    outcommented again

--- PM.EF/Team.cs
+++ PM.EF/Team.cs
@@ -21,7 +21,7 @@ namespace PM.EF
         }
     
         public int Id { get; set; }
-       // public string Name { get; set; }
+        //public string Name { get; set; }
         public int ProjectId { get; set; }
     
         public virtual Project Project { get; set; }
